7e6a1074d5968f0cf067e09965472a4598263500
doyoung <ldy9212@gmail.com>
2023-09-27 10:14:06 +0900

    chore: update some libs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
-import './global.css'
-import { ReactNode } from "react"
+import './global.css';
 
-const Layout = ({children}:{children:ReactNode})=>{
-    return(
+import { ReactNode } from 'react';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+    return (
         <html lang="ko">
             <body>
-                <main>
-                    {children}
-                </main>
+                <main>{children}</main>
             </body>
         </html>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
